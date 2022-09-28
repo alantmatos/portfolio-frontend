f8614e7099f8ca9732a052d535081108ec5731ec
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-09-28 12:01:23 -0400

    font awesome lib, footer, home page animation, color changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faYoutube, faLinkedin, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
@@ -8,10 +10,10 @@ const Footer = () => {
       <h3> Alan T Matos</h3>
       <p> expect something here... </p>
       <ul className='socials'>
-        <li><a href="#">Linkedin</a></li>
-        <li><a href="#">GitHub</a></li>
-        <li><a href="#">Twitter</a></li>
-        <li><a href="#">Facebook</a> </li>
+        <li><a href="https://www.linkedin.com/in/alantmatos/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></li>
+        <li><a href="https://www.instagram.com/sir._.robot/"><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a></li>
+        <li><a href="https://www.facebook.com/alantmatos/"> <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon> </a> </li>
 
       </ul>
     </div>
