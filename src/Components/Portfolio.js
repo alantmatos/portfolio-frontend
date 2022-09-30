d15817e576cc2781159e8ac88a2116d2302c133b
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-09-30 11:50:54 -0400

    portfolio page css updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -28,10 +28,10 @@ const Portfolio = () => {
   const renderProjects = projects.map(project => {
     return (
       <div className='project-info' key={project.id} onClick={(e) => manageDisplay(e)}>
-        <h2>{project.project_name} </h2>
+        <div className='project-name'><h2>{project.project_name} </h2></div>
         <div className='project-lang'><h4>{project.project_languages} </h4></div>
         <div className='project-description'><h4>{project.project_description} </h4></div>
-        <img className="project-cover" src={project.project_cover}></img>
+        <div className="project-cover" ><img src={project.project_cover}></img></div>
       </div>
     )
   })
@@ -40,13 +40,9 @@ const Portfolio = () => {
 
 
   return (
-    <>
-      {displayModal ? <Modal></Modal> : null}
-
       <div className='portfolio'>
         {displayProjects ? renderProjects : null}
       </div>
-    </>
   )
 }
 
