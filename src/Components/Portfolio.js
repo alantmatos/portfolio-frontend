e3350877ba4f0d1dbe69d6827f9eb7838eec1d2c
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-09-30 13:23:33 -0400

    portfolio css update, project_images pulled from backend, project_cover fixed

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -28,10 +28,17 @@ const Portfolio = () => {
   const renderProjects = projects.map(project => {
     return (
       <div className='project-info' key={project.id} onClick={(e) => manageDisplay(e)}>
-        <div className='project-name'><h2>{project.project_name} </h2></div>
-        <div className='project-lang'><h4>{project.project_languages} </h4></div>
-        <div className='project-description'><h4>{project.project_description} </h4></div>
-        <div className="project-cover" ><img src={project.project_cover}></img></div>
+        <div className='data-container'>
+          <div className='project-name'><h2>{project.project_name} </h2></div>
+          <div className='project-languages'><h4>{project.project_languages} </h4></div>
+          <div className='project-description'><h4>{project.project_description} </h4></div>
+        </div>
+        <div className='images-container'>
+          <div className="project-cover" ><img src={project.project_cover}></img></div>
+        </div>
+        <div className='prints-container'> {project.project_images.map(image => {return ( <div key={image.id} className='prints-frame'> <img className='prints' src={image.image_link}></img></div>)}) }</div>
+
+
       </div>
     )
   })
