075ee559ab05a2d910d9c68188f9e5c1f297de23
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-10-05 19:46:14 -0400

    resume link fix, css fix, margin home btn fix

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
       <div className='contact-info'>
         <h4>mr.alanmatos@hotmail.com</h4>
         <h5>+1 929-895-1957</h5>
-        <button> My Resume </button>
+        <a href="https://docs.google.com/document/d/1irITA5YLrJ_oSdZyekYTLvdqinvSC3ifHY4lYk8fMMg/edit?usp=sharing"> My Resume </a>
       </div>
     </div>
   )
