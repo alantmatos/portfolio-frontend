d903d4fe188aa27fda7895ad7db292cebfda290b
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-11-03 13:52:26 -0400

    update display project

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,6 +5,7 @@ import '../Css/portfolio.css';
 const Portfolio = () => {
 
   const [projects, setProjects] = useState([]);
+  const [count, setCount] = useState(0);
 
   useEffect(() => { fetchData() }, [])
 
@@ -18,12 +19,24 @@ const Portfolio = () => {
   //     .then(data => data.json())
   //     .then(data => setProjects(data))
   // };
-  
+
   const fetchData = () => {
     fetch('https://zany-gray-cod-hat.cyclic.app/projects')
       .then(data => data.json())
       .then(data => setProjects(data))
   };
+
+  const handleImageDisplay = (project) => {
+    let limit = project.project_images.length;
+
+    if (count <= limit - 1) {
+      setCount(count + 1)
+      let image = document.getElementById('active-image')
+      image.setAttribute('src', project.project_images[count])
+    } else {
+      setCount(0)
+    }
+  }
 
 
   const renderProjects = projects.map(project => {
@@ -33,15 +46,15 @@ const Portfolio = () => {
           <div className='project-name'><h2>{project.project_name} </h2></div>
           <div className='project-languages'><h4>{project.project_languages} </h4></div>
           <div className='project-description'><h4>{project.project_description} </h4></div>
-            <div className='buttons'>
-              { project.project_repo ? <a href={project.project_repo} target="_blank" rel="noreferrer"> <button className='portfolio-btn'> Demo </button></a> : null }
-              { project.project_demo ? <a href={project.project_demo} target="_blank" rel="noreferrer"> <button className='portfolio-btn'> Github Repo </button></a> : null }
-            </div>
+          <div className='buttons'>
+            {project.project_repo ? <a href={project.project_repo} target="_blank" rel="noreferrer"> <button className='portfolio-btn'> Demo </button></a> : null}
+            {project.project_demo ? <a href={project.project_demo} target="_blank" rel="noreferrer"> <button className='portfolio-btn'> Github Repo </button></a> : null}
+          </div>
         </div>
         <div className='images-container'>
-          <div className="project-cover" ><img src={project.project_cover}></img></div>
+          <div className="project-cover" ><img id="active-image" onClick={() => { handleImageDisplay(project) }} src={project.project_cover}></img></div>
         </div>
-        <div className='prints-container'> {project.project_images.map(image => { return (<div key={image.id} className='prints-frame'> <img className='prints' src={image.image_link}></img></div>) })}</div>
+        {/* <div className='prints-container'> {project.project_images.map(image => { return (<div key={image} className='prints-frame'> <img className='prints' src={image}></img></div>) })}</div> */}
       </div>
     )
   })
